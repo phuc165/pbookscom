e46208d8c2f79c8f1dc2d9c698adf983674495db
phuc165 <65102747+phuc165@users.noreply.github.com>
2024-09-02 01:24:26 +0700

    r

---
+++ src/Button/index.js
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import styles from '~/Button/Button.module.css'
+
+function Button({ primary, warning, danger }) {
+    const classes = clsx(styles.btn, {
+        [styles.primary] : primary,
+        [styles.warning]: warning,
+        [styles.danger]: danger
+    })
+    return (  
+        <button className={classes}>
+            Bam tao!
+        </button>
+    );
+}
+
+export default Button;
---
+++ src/page/Home/index.js
@@ -0,0 +1,13 @@
+import Button from "~/Button";
+function Home() {
+    return (
+        <>
+            <h2>Home page</h2>
+            <Button primary/>
+            <Button warning/>
+            <Button danger/>
+        </>
+    );
+}
+
+export default Home;
